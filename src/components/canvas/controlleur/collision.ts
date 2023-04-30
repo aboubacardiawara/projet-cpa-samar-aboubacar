46c14534fc7faf146f44e8ff29aa6f67dca9bf08
waradia <aboubacardiawara909@gmail.com>
2023-04-30 16:51:41 +0200

    comment collision function

--- src/components/canvas/controlleur/collision.ts
+++ src/components/canvas/controlleur/collision.ts
@@ -3,16 +3,97 @@ import { HEIGHT_SORTIE, RADIUS, SIZE_RESSOURCE, TAILLE_ENEMIE_IMMOBILE, TAILLE_E
 import { isImobileEnemie } from "./enemie";
 import { Ressource } from "./ressource";
 import { Enemie, Sortie, Wall } from "./state";
+type Box = { x: number, y: number, width: number, height: number }
 
 /**
  * 
  * @author Prof
+ * @param ball {Ball}
+ * @param box {Box}
+ * @returns {boolean} true si il y a collision
+ */
+const collisionBallBox = (ball: Ball, box: Box): boolean => {
+  const circle = { x: ball.coord.x, y: ball.coord.y, radius: RADIUS }
+  let distX = Math.abs(circle.x - box.x - box.width / 2)
+  let distY = Math.abs(circle.y - box.y - box.height / 2)
+
+  if (distX > (box.width / 2 + circle.radius)) {
+    return false
+  }
+  if (distY > (box.height / 2 + circle.radius)) {
+    return false
+  }
+  if (distX <= (box.width / 2) || distY <= (box.height / 2)) {
+    return true
+  }
+
+  let dx = distX - box.width / 2
+  let dy = distY - box.height / 2
+  return (dx * dx + dy * dy <= (circle.radius * circle.radius))
+
+}
+
+/**
+ * Convertie un enemie en box
+ * @param enemie {Enemie}
+ * @returns {Box} 
+ */
+const enemieAsBox = (enemie: Enemie): Box => {
+  return {
+    x: enemie.coord.x,
+    y: enemie.coord.y,
+    width: isImobileEnemie(enemie) ? TAILLE_ENEMIE_IMMOBILE.w : TAILLE_ENEMIE_MOBILE,
+    height: isImobileEnemie(enemie) ? TAILLE_ENEMIE_IMMOBILE.h : TAILLE_ENEMIE_MOBILE
+  }
+}
+
+/**
+ * Convertie une sortie en box
+ * @param sortie {Sortie} 
+ * @returns {Box}
+ */
+const sortieAsBox = (sortie: Sortie): Box => {
+  return {
+    x: sortie.position.x,
+    y: sortie.position.y,
+    width: WIDTH_SORTIE,
+    height: HEIGHT_SORTIE
+  }
+}
+
+/**
+ * Convertie une ressource en box
+ * @param ressource {Ressource} 
+ * @returns {Box}
+ */
+const ressourceAsBox = (ressource: Ressource): Box => {
+  return {
+    x: ressource.position.x,
+    y: ressource.position.y,
+    width: SIZE_RESSOURCE,
+    height: SIZE_RESSOURCE
+  }
+}
+
+
+/**
+ * Verifie la collision entre la balle et un enemie
  * @param ball 
- * @param rec 
+ * @param enemie 
  * @returns 
  */
-const collisionCircleBoxProf2 = (ball: Ball, rec: Wall): boolean => {
+export const collisionBallEnemie = (ball: Ball, enemie: Enemie): boolean => {
+  return collisionBallBox(ball, enemieAsBox(enemie))
+}
 
+/**
+ * On réutiliser la correction de la collision entre une balle et un mur
+ * du TP1.
+ * @param ball {Ball}
+ * @param rec  {Wall}
+ * @returns  {boolean} true si il y a collision
+ */
+export const collisionCircleBox = (ball: Ball, rec: Wall): boolean => {
   const circle = { x: ball.coord.x, y: ball.coord.y, radius: RADIUS }
   const box = { x: rec.position.x, y: rec.position.y, width: rec.width, height: rec.height }
   if (circle.x + circle.radius > box.x && circle.x < box.x) {
@@ -40,88 +121,22 @@ const collisionCircleBoxProf2 = (ball: Ball, rec: Wall): boolean => {
   return false;
 }
 
+/**
+ * Teste la collision entre la balle et la sortie
+ * @param ball { Ball}
+ * @param exit { Sortie}
+ * @returns  { boolean} true si collision
+ */
+export const collisionCircleExit = (ball: Ball, exit: Sortie): boolean => {
+  return collisionBallBox(ball, sortieAsBox(exit))
+}
 
 /**
- * Verifie la collision entre la balle et un enemie
- * @param ball 
- * @param enemie 
- * @returns 
+ * Teste la collision entre la balle et une ressource
+ * @param ball { Ball} 
+ * @param ressource { Ressource}
+ * @returns { boolean} true si collision
  */
-export const collisionBallEnemie = (ball: Ball, enemie: Enemie): boolean => {
-  const circle = { x: ball.coord.x, y: ball.coord.y, radius: RADIUS }
-  const box = {
-    x: enemie.coord.x,
-    y: enemie.coord.y,
-    width: isImobileEnemie(enemie) ? TAILLE_ENEMIE_IMMOBILE.w : TAILLE_ENEMIE_MOBILE,
-    height: isImobileEnemie(enemie) ? TAILLE_ENEMIE_IMMOBILE.h : TAILLE_ENEMIE_MOBILE
-  }
-
-  let distX = Math.abs(circle.x - box.x - box.width / 2)
-  let distY = Math.abs(circle.y - box.y - box.height / 2)
-
-  if (distX > (box.width / 2 + circle.radius)) {
-    return false
-  }
-  if (distY > (box.height / 2 + circle.radius)) {
-    return false
-  }
-  if (distX <= (box.width / 2) || distY <= (box.height / 2)) {
-    return true
-  }
-
-  let dx = distX - box.width / 2
-  let dy = distY - box.height / 2
-  return (dx * dx + dy * dy <= (circle.radius * circle.radius))
-}
-
-export const collisionBallRect = (
-  ball: Ball,
-  { rectx, recty, rectw, recth }
-    : { rectx: number, recty: number, rectw: number, recth: number }
-): boolean => {
-  const circle = { x: ball.coord.x, y: ball.coord.y, radius: RADIUS }
-  const box = { x: rectx, y: recty, width: rectw, height: recth }
-
-  let distX = Math.abs(circle.x - box.x - box.width / 2)
-  let distY = Math.abs(circle.y - box.y - box.height / 2)
-
-  if (distX > (box.width / 2 + circle.radius)) {
-    return false
-  }
-  if (distY > (box.height / 2 + circle.radius)) {
-    return false
-  }
-  if (distX <= (box.width / 2) || distY <= (box.height / 2)) {
-    return true
-  }
-
-  let dx = distX - box.width / 2
-  let dy = distY - box.height / 2
-  return (dx * dx + dy * dy <= (circle.radius * circle.radius))
-}
-
-
-export const collisionCircleBox = (ball: Ball, rec: Wall): boolean => {
-  return collisionCircleBoxProf(ball, rec)
-}
-
-export const collisionCircleExit = (ball: Ball, exit: Sortie): boolean => {
-  const wall: Wall = {
-    ...exit,
-    width: WIDTH_SORTIE,
-    height: HEIGHT_SORTIE,
-  }
-
-  return collisionCircleBoxProf(ball, wall)
-}
-
 export const collisionBallRessource = (ball: Ball, ressource: Ressource): boolean => {
-  const ressourceAsBox = {
-    rectx: ressource.position.x,
-    recty: ressource.position.y,
-    rectw: SIZE_RESSOURCE,
-    recth: SIZE_RESSOURCE
-  }
-
-  return collisionBallRect(ball, ressourceAsBox);
+  return collisionBallBox(ball, ressourceAsBox(ressource))
 }
