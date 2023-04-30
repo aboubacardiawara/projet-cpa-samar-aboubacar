2273c2adfae0fb1c334d02ece1bc28f58662b034
waradia <aboubacardiawara909@gmail.com>
2023-04-30 17:24:23 +0200

    comment ressource

--- src/components/canvas/controlleur/ressource.ts
+++ src/components/canvas/controlleur/ressource.ts
@@ -7,7 +7,13 @@ export type Ressource = {
     imgIndex: number
 }
 
-
+/**
+ * Bouge une ressource d'une etape
+ * Une ressource etant immobile, elle ne bouge pas.
+ * Juste son image change pour l'animation.
+ * @param ressource0 
+ * @returns 
+ */
 export const nextStepRessource = (ressource0: Ressource): Ressource => {
     const ressource: Ressource = ressource0
     if (ressource.cpt == 25) {
